10c1b78d0d94e5ae556b2f07b95f97ba676482ca
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2012-07-19 13:29:54 +0000

    stuff

---
+++ trunk/src/morda/resman/Resource.hpp
@@ -0,0 +1,78 @@
+/* The MIT License:
+
+Copyright (c) 2012 Ivan Gagis <igagis@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+// Home page: http://morda.googlecode.com
+
+/**
+ * @author Ivan Gagis <igagis@gmail.com>
+ */
+
+#pragma once
+
+#include <map>
+
+#include <ting/Ref.hpp>
+
+
+
+namespace morda{
+
+//forward declarations
+class ResMan;
+
+
+//base class for all resources
+class Resource : virtual public ting::RefCounted{
+	friend class ResMan;
+
+	struct StringComparator{
+		bool operator()(const std::string* s1, const std::string* s2)const{
+			return s1->compare(*s2) < 0;
+		}
+	};
+	typedef std::map<const std::string*, ting::WeakRef<Resource>, StringComparator> T_ResMap;
+
+	//ResMapRC = Resource Map RefCounted
+	class ResMapRC : public RefCounted{
+	public:
+		~ResMapRC()throw(){}
+		
+		T_ResMap rm;
+
+		static inline ting::Ref<ResMapRC> New(){
+			return ting::Ref<ResMapRC>(new ResMapRC());
+		}
+	};
+
+	ting::WeakRef<ResMapRC> rm;
+	T_ResMap::iterator it;
+
+protected:
+	//this can only be used as a base class
+	inline Resource(){}
+public:
+	virtual ~Resource()throw();
+};
+
+
+
+}//~namespace
