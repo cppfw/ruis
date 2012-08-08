40ea001957d7d37d2ad0c0a95e55196b36bdd190
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2012-08-08 08:41:31 +0000

    TexFont refactoring

--- trunk/src/morda/util/TexFont.hpp
+++ trunk/src/morda/util/TexFont.hpp
@@ -34,6 +34,7 @@ THE SOFTWARE. */
 #include <stdexcept>
 
 #include <tride/Vector3.hpp>
+#include <tride/Rectangle2.hpp>
 
 #include <ting/types.hpp>
 #include <ting/Exc.hpp>
@@ -67,32 +68,10 @@ class TexFont{
 	float fontSize;
 
 public:
-	class BoundingBox{
-	public:
-		float left;
-		float right;
-		float top;
-		float bottom;
-
-		inline float Width()const{
-			return ASSCOND(this->left + this->right, >= 0);
-		}
-
-		inline float Height()const{
-			return ASSCOND(this->top + this->bottom, >= 0);
-		}
-
-		inline void SetToZero(){
-			this->left = 0;
-			this->right = 0;
-			this->top = 0;
-			this->bottom = 0;
-		}
-	};
 
 private:
 	//Bounding box holds the dimensions of the largest loaded glyph.
-	BoundingBox boundingBox;//TODO: use Rectangle2
+	tride::Rect2f boundingBox;
 
 public:
 	TexFont(){}
@@ -107,158 +86,77 @@ public:
 		return this->fontSize;
 	}
 
-	//renders the string, returns resulting string length
-	//TODO:return string bounding box
+	//renders the string, returns resulting string advance
 	inline float RenderString(TexturingShader& shader, const tride::Matr4f& matrix, const wchar_t* s)const{
-		return this->RenderStringInternal<wchar_t>(shader, matrix, s);
+		return this->RenderStringInternal(shader, matrix, s);
 	}
 
 	inline float RenderString(TexturingShader& shader, const tride::Matr4f& matrix, const std::wstring& s)const{
-		return this->RenderStringInternal<wchar_t>(shader, matrix, s.c_str());
+		return this->RenderStringInternal(shader, matrix, s.c_str());
 	}
 
 	inline float RenderString(TexturingShader& shader, const tride::Matr4f& matrix, const char* s)const{
-		return this->RenderStringInternal<char>(shader, matrix, s);
+		return this->RenderStringInternal(shader, matrix, s);
 	}
 
 	inline float RenderString(TexturingShader& shader, const tride::Matr4f& matrix, const std::string& s)const{
-		return this->RenderStringInternal<char>(shader, matrix, s.c_str());
+		return this->RenderStringInternal(shader, matrix, s.c_str());
 	}
 
-	inline const BoundingBox& FontBoundingBox()const{
+	inline const tride::Rect2f& FontBoundingBox()const{
 		return this->boundingBox;
 	}
 
-	inline float StringWidth(const wchar_t* s)const{
-		return this->StringWidthInternal<wchar_t>(s);
+	inline float StringAdvance(const wchar_t* s)const{
+		return this->StringAdvanceInternal(s);
+	}
+	
+	inline float StringAdvance(const std::wstring& s)const{
+		return this->StringAdvanceInternal(s.c_str());
+	}
+	
+	inline float StringAdvance(const char* s)const{
+		return this->StringAdvanceInternal(s);
+	}
+	
+	inline float StringAdvance(const std::string& s)const{
+		return this->StringAdvanceInternal(s.c_str());
 	}
 
-	inline BoundingBox StringBoundingBox(const wchar_t* s)const{
-		return this->StringBoundingBoxInternal<wchar_t>(s);
+	inline tride::Rect2f StringBoundingBox(const wchar_t* s)const{
+		return this->StringBoundingBoxInternal(s);
+	}
+	
+	inline tride::Rect2f StringBoundingBox(const std::wstring& s)const{
+		return this->StringBoundingBoxInternal(s.c_str());
 	}
 
-	inline BoundingBox StringBoundingBox(const char* s)const{
-		return this->StringBoundingBoxInternal<char>(s);
+	inline tride::Rect2f StringBoundingBox(const char* s)const{
+		return this->StringBoundingBoxInternal(s);
 	}
 
-	inline BoundingBox StringBoundingBox(const std::string& s)const{
-		return this->StringBoundingBoxInternal<char>(s.c_str());
+	inline tride::Rect2f StringBoundingBox(const std::string& s)const{
+		return this->StringBoundingBoxInternal(s.c_str());
 	}
 
 	DEBUG_CODE( void RenderTex(TexturingShader& shader, const tride::Matr4f& matrix)const; )
 
 private:
 
-	template <class T> float StringWidthInternal(const T* s)const{
-		float ret = 0;
-
-		try{
-			for(; *s != 0; ++s){
-				const Glyph& g = this->glyphs.at(wchar_t(*s));
-				ret += g.advance;
-			}
-		}catch(std::out_of_range& e){
-			std::stringstream ss;
-			ss << "TexFont::StringWidthInternal(): Character is not loaded, scan code = 0x" << std::hex << *s;
-			throw ting::Exc(ss.str().c_str());
-		}
-
-		return ret;
-	}
-
-	template <class T> BoundingBox StringBoundingBoxInternal(const T* s)const{
-		BoundingBox ret;
-
-		if(*s == 0){
-			ret.SetToZero();
-			return ret;
-		}
-
-		float curAdvance = 0;
-
-		//init ret with bounding box of the first glyph
-		{
-			const Glyph& g = this->glyphs.at(wchar_t(*s));
-			ret.left = g.verts[0].x;
-			ret.right = g.verts[2].x;
-			ret.top = g.verts[1].y;
-			ret.bottom = g.verts[0].y;
-			curAdvance += g.advance;
-			++s;
-		}
-
-		try{
-			for(; *s != 0; ++s){
-				const Glyph& g = this->glyphs.at(wchar_t(*s));
-
-				if(g.verts[1].y > ret.top){
-					ret.top = g.verts[1].y;
-				}
-				
-				if(g.verts[0].y < ret.bottom){
-					ret.bottom = - g.verts[0].y;
-				}
-
-				if(curAdvance + g.verts[0].x < ret.left){
-					ret.left = -(curAdvance + g.verts[0].x);
-				}
-
-				if(curAdvance + g.verts[2].x > ret.right){
-					ret.right = curAdvance + g.verts[2].x;
-				}
-
-				curAdvance += g.advance;
-			}
-		}catch(std::out_of_range& e){
-			std::stringstream ss;
-			ss << "TexFont::StringBoundingLineInternal(): Character is not loaded, scan code = 0x" << std::hex << *s;
-			throw ting::Exc(ss.str().c_str());
-		}
-
-		ASSERT(ret.Width() >= 0)
-		ASSERT(ret.Height() >= 0)
-		return ret;
-	}
-
-	inline float RenderGlyphInternal(TexturingShader& shader, const tride::Matr4f& matrix, wchar_t ch)const{
-		const Glyph& g = this->glyphs.at(ch);
-
-		shader.SetMatrix(matrix);
-
-		shader.SetPositionPointer(g.verts.Begin());
-		shader.SetTexCoordPointer(g.texCoords.Begin());
-
-		shader.DrawArrays(GL_TRIANGLE_FAN, g.verts.Size());
-
-		return g.advance;
-	}
-
-
+	float StringAdvanceInternal(const char* s)const;
 	
-	template <class T> float RenderStringInternal(TexturingShader& shader, const tride::Matr4f& matrix, const T* s)const{
-		shader.EnablePositionPointer();
-		shader.EnableTexCoordPointer();
+	float StringAdvanceInternal(const wchar_t* s)const;
 
-		this->tex.Bind();
+	tride::Rect2f StringBoundingBoxInternal(const char* s)const;
+	
+	tride::Rect2f StringBoundingBoxInternal(const wchar_t* s)const;
+	
+	float RenderStringInternal(TexturingShader& shader, const tride::Matr4f& matrix, const char* s)const;
+	
+	float RenderStringInternal(TexturingShader& shader, const tride::Matr4f& matrix, const wchar_t* s)const;	
 
-		float ret = 0;
+	float RenderGlyphInternal(TexturingShader& shader, const tride::Matr4f& matrix, wchar_t ch)const;
 
-		tride::Matr4f matr(matrix);
-
-		try{
-			for(; *s != 0; ++s){
-				float advance = this->RenderGlyphInternal(shader, matr, wchar_t(*s));
-				ret += advance;
-				matr.Translate(advance, 0);
-			}
-		}catch(std::out_of_range& e){
-			std::stringstream ss;
-			ss << "TexFont::RenderStringInternal(): Character is not loaded, scan code = 0x" << std::hex << *s;
-			throw ting::Exc(ss.str());
-		}
-
-		return ret;
-	}
 };//~class TexFont
 
 
