0fd6de76d570e5c8b013c35caa93f1b99d611a82
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2012-08-14 19:45:00 +0000

    ComputeMinSize method renamed

--- src/morda/Widget.hpp
+++ src/morda/Widget.hpp
@@ -170,7 +170,7 @@ public:
 //		TRACE(<< "Widget::OnResize(): invoked" << std::endl)
 	}
 	
-	virtual tride::Vec2f ComputeMinSize()const{
+	virtual tride::Vec2f ComputeMinimalDimensions()const{
 		return tride::Vec2f(0, 0);
 	}
 
