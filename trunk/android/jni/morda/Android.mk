922b5457941791ca7c05a194f695ac510d3864a9
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2012-08-23 17:51:11 +0000

    android files

---
+++ trunk/android/jni/morda/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+#TARGET_BUILD_TYPE := debug
+
+LOCAL_MODULE    := cofi
+
+LOCAL_SRC_FILES := main.cpp
+#LOCAL_SRC_FILES += ../AssetFile/AssetFile.cpp
+#LOCAL_SRC_FILES += Activity.cpp
+
+LOCAL_CFLAGS := -Wno-div-by-zero #disable integer division by zero warning as it is sometimes useful when working with templates
+LOCAL_CFLAGS += -DDEBUG
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/.. $(LOCAL_PATH)/../ting $(LOCAL_PATH)/../aumiks
+
+LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv1_CM
+
+LOCAL_SHARED_LIBRARIES := ting
+
+include $(BUILD_SHARED_LIBRARY)
