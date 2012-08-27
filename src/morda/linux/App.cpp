c90a054bf217fe7ecdd77f20826cf142127d5cdb
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2012-08-27 13:32:58 +0000

    stuff

--- src/morda/linux/App.cpp
+++ src/morda/linux/App.cpp
@@ -297,8 +297,6 @@ namespace morda{
 inline void Main(int argc, char** argv){
 	ting::Ptr<morda::App> app = morda::CreateApp(argc, argv, ting::Buffer<const ting::u8>(0, 0));
 
-	app->Init();
-
 	app->Exec();
 }
 }//~namespace
