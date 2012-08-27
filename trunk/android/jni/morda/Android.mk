3cc935f8f8024b9544ddca933b59a25ca4b3bf4c
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2012-08-27 15:16:27 +0000

    external libs for android

--- trunk/android/jni/morda/Android.mk
+++ trunk/android/jni/morda/Android.mk
@@ -33,11 +33,18 @@ LOCAL_SRC_FILES += morda/layouts/LinearLayout.cpp
 LOCAL_CFLAGS := -Wno-div-by-zero #disable integer division by zero warning as it is sometimes useful when working with templates
 LOCAL_CFLAGS += -DDEBUG
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ting $(LOCAL_PATH)/../stob $(LOCAL_PATH)/../tride
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ting
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../stob
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../tride
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libpng
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libjpeg/jpeg
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libfreetype2/include
 
-LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv1_CM
+LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2 -lz
 
 LOCAL_SHARED_LIBRARIES := ting stob
+
+LOCAL_STATIC_LIBRARIES := png jpeg freetype2
 
 #disable undefined symbols check because we require CreateApp() function to be defined by library user
 LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
