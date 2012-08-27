d813050f80a662131e0a98e7a92892dfcfc5ec70
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2012-08-27 10:55:09 +0000

    stuff

--- trunk/android/jni/morda/Android.mk
+++ trunk/android/jni/morda/Android.mk
@@ -8,6 +8,26 @@ LOCAL_MODULE := morda
 
 LOCAL_SRC_FILES := morda/android/App.cpp
 LOCAL_SRC_FILES += morda/android/AssetFile.cpp
+LOCAL_SRC_FILES += morda/App.cpp
+LOCAL_SRC_FILES += morda/Widget.cpp
+LOCAL_SRC_FILES += morda/Container.cpp
+LOCAL_SRC_FILES += morda/GuiInflater.cpp
+LOCAL_SRC_FILES += morda/shaders/Shader.cpp
+LOCAL_SRC_FILES += morda/shaders/SimpleSingleColoringShader.cpp
+LOCAL_SRC_FILES += morda/shaders/TexturingShader.cpp
+LOCAL_SRC_FILES += morda/shaders/SimpleTexturingShader.cpp
+LOCAL_SRC_FILES += morda/resman/ResourceManager.cpp
+LOCAL_SRC_FILES += morda/resman/Resource.cpp
+LOCAL_SRC_FILES += morda/resources/ResTexture.cpp
+LOCAL_SRC_FILES += morda/resources/ResFont.cpp
+LOCAL_SRC_FILES += morda/util/GLTexture.cpp
+LOCAL_SRC_FILES += morda/util/Image.cpp
+LOCAL_SRC_FILES += morda/util/TexFont.cpp
+LOCAL_SRC_FILES += morda/util/util.cpp
+LOCAL_SRC_FILES += morda/widgets/AbstractButton.cpp
+LOCAL_SRC_FILES += morda/widgets/Button.cpp
+LOCAL_SRC_FILES += morda/widgets/Label.cpp
+LOCAL_SRC_FILES += morda/layouts/LinearLayout.cpp
 
 
 LOCAL_CFLAGS := -Wno-div-by-zero #disable integer division by zero warning as it is sometimes useful when working with templates
@@ -18,5 +38,8 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ting $(LOCAL_PATH)/../stob $(LOCAL_PATH)/..
 LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv1_CM
 
 LOCAL_SHARED_LIBRARIES := ting stob
+
+#disable undefined symbols check because we require CreateApp() function to be defined by library user
+LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
 
 include $(BUILD_SHARED_LIBRARY)
