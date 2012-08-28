cfcd7f97bc56a5fafd74f9aaaee40b6cc1dd8316
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2012-08-28 11:38:54 +0000

    stuff

---
+++ android/jni/tests/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+#TARGET_BUILD_TYPE := debug
+
+LOCAL_MODULE := tests
+
+LOCAL_SRC_FILES := src/main.cpp
+
+
+LOCAL_CFLAGS := -Wno-div-by-zero #disable integer division by zero warning as it is sometimes useful when working with templates
+LOCAL_CFLAGS += -DDEBUG
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../morda
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../ting
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../tride
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../stob
+
+LOCAL_LDLIBS := -llog -landroid -lGLESv2
+
+LOCAL_SHARED_LIBRARIES := morda stob ting
+
+LOCAL_STATIC_LIBRARIES := 
+
+#disable undefined symbols check because we require CreateApp() function to be defined by library user
+#LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
+
+include $(BUILD_SHARED_LIBRARY)
