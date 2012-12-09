2ae5ad1a9a8978768ff4a17a7cf56fce4e0d2b64
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2012-12-09 22:51:03 +0000

    stuff

--- android/src/com/googlecode/morda/tests/SharedLibLoaderNativeActivity.java
+++ android/src/com/googlecode/morda/tests/SharedLibLoaderNativeActivity.java
@@ -1,6 +1,7 @@
 package com.googlecode.morda.tests;
 
 import android.app.NativeActivity;
+import android.view.KeyCharacterMap;
 
 public class SharedLibLoaderNativeActivity extends NativeActivity {
 
@@ -13,7 +14,19 @@ static {
 }
 
 
-	public int resolveKeyUnicode(int deviceID, int keyCode, int metastate){
-		return 0x30;//TODO:
+	private int curKeyDevice;
+	private KeyCharacterMap curCharMap;
+
+	public int resolveKeyUnicode(int deviceID, int keyCode, int metaState){
+		if(this.curCharMap == null || curKeyDevice != deviceID){
+			try{
+				this.curCharMap = KeyCharacterMap.load(deviceID);
+				this.curKeyDevice = deviceID;
+			}catch(KeyCharacterMap.UnavailableException e){
+				return 0;//could not load char map, thus could not resolve character
+			}
+		}
+		
+		return this.curCharMap.get(keyCode, metaState);
 	}
 }
