d3278533f9fe3d0cf9fc2de100858d501d30f7d1
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2012-12-11 17:51:08 +0000

    stuff

--- trunk/android/src/com/googlecode/morda/tests/SharedLibLoaderNativeActivity.java
+++ trunk/android/src/com/googlecode/morda/tests/SharedLibLoaderNativeActivity.java
@@ -2,9 +2,11 @@ package com.googlecode.morda.tests;
 
 import android.app.NativeActivity;
 import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
 import android.util.Log;
 
 public class SharedLibLoaderNativeActivity extends NativeActivity {
+	public static final String LOGTAG = "morda java side";
 
 static {
 	System.loadLibrary("gnustl_shared");
@@ -24,11 +26,26 @@ static {
 				this.curCharMap = KeyCharacterMap.load(deviceID);
 				this.curKeyDevice = deviceID;
 			}catch(Exception e){
-				Log.e("morda java side", "Could not load KeyCharacterMap");
+				Log.e(LOGTAG, "resolveKeyUnicode(): Could not load KeyCharacterMap");
 				return 0;//could not load char map, thus could not resolve character
 			}
 		}
 		
 		return this.curCharMap.get(keyCode, metaState);
+	}
+	
+	
+	
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent event) {
+		Log.d(LOGTAG, "dispatchKeyEvent(): invoked");
+		if(event.getAction() == KeyEvent.ACTION_MULTIPLE && event.getKeyCode() == KeyEvent.KEYCODE_UNKNOWN){
+			Log.d(LOGTAG, "dispatchKeyEvent(): multiple characters are being delivered, chars = " + event.getCharacters());
+			//Multiple characters are being delivered. NativeActivity currently does not
+			//support passing multiple characters from KeyEvent to native code, so we need to do it here.
+			//TODO:
+			return true;
+		}
+		return super.dispatchKeyEvent(event);
 	}
 }
