c26f408616a04ddc828dcd64864bddaf9344b555
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2013-09-10 14:57:32 +0000

    stuff

--- android/tests/src/com/googlecode/morda_tests/TestActivity.java
+++ android/tests/src/com/googlecode/morda_tests/TestActivity.java
@@ -3,13 +3,14 @@ package com.googlecode.morda_tests;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class TestActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import com.googlecode.morda.MordaActivity;
+
+public class TestActivity extends MordaActivity{
+
+	{
+		System.loadLibrary("gnustl_shared");
+		System.loadLibrary("tests");
+	}
+
+
 }
