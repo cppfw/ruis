6b028c5b5a48ab805e43bd739d13041f8dfd2b8e
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2013-09-11 08:10:19 +0000

    stuff

--- trunk/android/tests/src/com/googlecode/morda_tests/TestActivity.java
+++ trunk/android/tests/src/com/googlecode/morda_tests/TestActivity.java
@@ -2,15 +2,22 @@ package com.googlecode.morda_tests;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.googlecode.morda.MordaActivity;
 
 public class TestActivity extends MordaActivity{
+	public static final String LOGTAG = "morda_tests java side";
 
 	{
+		Log.d(LOGTAG, "Loading native libraries");
 		System.loadLibrary("gnustl_shared");
 		System.loadLibrary("tests");
+		Log.d(LOGTAG, "Native libraries loaded");
 	}
 
-
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+	}
 }
