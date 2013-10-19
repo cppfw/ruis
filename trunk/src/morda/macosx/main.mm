c115c3c0225d0a02bb3d4d2fe8558c2e56177866
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2013-10-19 21:12:44 +0000

    stuff

--- trunk/src/morda/macosx/main.mm
+++ trunk/src/morda/macosx/main.mm
@@ -20,7 +20,7 @@
 	[[NSNotificationCenter defaultCenter]
 		postNotificationName:NSApplicationDidFinishLaunchingNotification
 		object:NSApp];
-	
+
 	shouldKeepRunning = YES;
 	do{
 		NSEvent *event =
@@ -29,7 +29,7 @@
 				untilDate:[NSDate distantFuture]
 				inMode:NSDefaultRunLoopMode
 				dequeue:YES];
-		
+
 		[self sendEvent:event];
 		[self updateWindows];
 	}while(shouldKeepRunning);
@@ -44,47 +44,48 @@
 
 
 int main (int argc, const char** argv){
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
 //    NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
 //    Class principalClass = NSClassFromString([infoDictionary objectForKey:@"NSPrincipalClass"]);
-    NSApplication *applicationObject = [[MordaApplication alloc] init];//[principalClass sharedApplication];
+	NSApplication *applicationObject = [[MordaApplication alloc] init];//[principalClass sharedApplication];
 
 //    NSString *mainNibName = [infoDictionary objectForKey:@"NSMainNibFile"];
 //    NSNib *mainNib = [[NSNib alloc] initWithNibNamed:mainNibName bundle:[NSBundle mainBundle]];
 //    [mainNib instantiateNibWithOwner:applicationObject topLevelObjects:nil];
 
-    [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-    id menubar = [[NSMenu new] autorelease];
-    id appMenuItem = [[NSMenuItem new] autorelease];
-    [menubar addItem:appMenuItem];
-    [NSApp setMainMenu:menubar];
-    id appMenu = [[NSMenu new] autorelease];
-    id appName = [[NSProcessInfo processInfo] processName];
-    id quitTitle = [@"Quit " stringByAppendingString:appName];
-    id quitMenuItem = [[[NSMenuItem alloc] initWithTitle:quitTitle
-        action:@selector(terminate:) keyEquivalent:@"q"] autorelease];
-    [appMenu addItem:quitMenuItem];
-    [appMenuItem setSubmenu:appMenu];
-    
-    id window = [[[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 200, 200)
-        styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:NO]
-            autorelease];
-    [window cascadeTopLeftFromPoint:NSMakePoint(20,20)];
-    [window setTitle:appName];
-    [window makeKeyAndOrderFront:nil];
-    [NSApp activateIgnoringOtherApps:YES];
-    
-    if ([applicationObject respondsToSelector:@selector(run)]){
-        [applicationObject
-                performSelectorOnMainThread:@selector(run)
-                withObject:nil
-                waitUntilDone:YES
-            ];
-    }
+	[NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+	id menubar = [[NSMenu new] autorelease];
+	id appMenuItem = [[NSMenuItem new] autorelease];
+	[menubar addItem:appMenuItem];
+	[NSApp setMainMenu:menubar];
+	id appMenu = [[NSMenu new] autorelease];
+	id appName = [[NSProcessInfo processInfo] processName];
+	id quitTitle = [@"Quit " stringByAppendingString:appName];
+	id quitMenuItem = [[[NSMenuItem alloc] initWithTitle:quitTitle
+		action:@selector(terminate:) keyEquivalent:@"q"] autorelease];
+	[appMenu addItem:quitMenuItem];
+	[appMenuItem setSubmenu:appMenu];
 
-//    [mainNib release];
-    [pool release];
+	NSWindow* window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 200, 200)
+		styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:NO];
+	[window cascadeTopLeftFromPoint:NSMakePoint(20, 20)];
+	[window setTitle:appName];
+	[window makeKeyAndOrderFront:nil];
+	[NSApp activateIgnoringOtherApps:YES];
 
-    return 0;
+	if ([applicationObject respondsToSelector:@selector(run)]){
+		[applicationObject
+				performSelectorOnMainThread:@selector(run)
+				withObject:nil
+				waitUntilDone:YES
+			];
+	}
+
+	[window release];
+//	[mainNib release];
+	[applicationObject release];
+	[pool release];
+
+	return 0;
 }
