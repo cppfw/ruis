55b82984d941a5d226901f70fe248e3b23dee84b
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2013-10-19 08:55:05 +0000

    macosx files

---
+++ trunk/src/morda/macosx/main.m
@@ -0,0 +1,27 @@
+#import <Cocoa/Cocoa.h>
+
+int main (int argc, char** argv){
+    [NSAutoreleasePool new];
+    [NSApplication sharedApplication];
+    [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+    id menubar = [[NSMenu new] autorelease];
+    id appMenuItem = [[NSMenuItem new] autorelease];
+    [menubar addItem:appMenuItem];
+    [NSApp setMainMenu:menubar];
+    id appMenu = [[NSMenu new] autorelease];
+    id appName = [[NSProcessInfo processInfo] processName];
+    id quitTitle = [@"Quit " stringByAppendingString:appName];
+    id quitMenuItem = [[[NSMenuItem alloc] initWithTitle:quitTitle
+        action:@selector(terminate:) keyEquivalent:@"q"] autorelease];
+    [appMenu addItem:quitMenuItem];
+    [appMenuItem setSubmenu:appMenu];
+    id window = [[[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 200, 200)
+        styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:NO]
+            autorelease];
+    [window cascadeTopLeftFromPoint:NSMakePoint(20,20)];
+    [window setTitle:appName];
+    [window makeKeyAndOrderFront:nil];
+    [NSApp activateIgnoringOtherApps:YES];
+    [NSApp run];
+    return 0;
+}
