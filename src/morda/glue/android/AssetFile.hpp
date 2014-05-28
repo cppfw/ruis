8c926c51a4804909ddb91d94e493e2c6dc1692f5
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2014-05-28 14:05:38 +0000

    stuff

--- src/morda/glue/android/AssetFile.hpp
+++ src/morda/glue/android/AssetFile.hpp
@@ -1,6 +1,6 @@
 /* The MIT License:
 
-Copyright (c) 2012 Ivan Gagis <igagis@gmail.com>
+Copyright (c) 2012-2014 Ivan Gagis <igagis@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -39,7 +39,7 @@ THE SOFTWARE. */
 #include <ting/Ptr.hpp>
 #include <ting/fs/File.hpp>
 
-#include "../App.hpp"
+#include "../../App.hpp"
 
 
 
