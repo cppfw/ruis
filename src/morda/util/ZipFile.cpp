70b8b6fc0b2132ba3985a8c2024482fec8ee3f3a
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2014-07-02 19:20:07 +0000

    stuff

--- src/morda/util/ZipFile.cpp
+++ src/morda/util/ZipFile.cpp
@@ -1,7 +1,8 @@
 #include "ZipFile.hpp"
 
-#include <unzip/unzip.h>
+#include "unzip/unzip.h"
 
+#include <sstream>
 
 
 using namespace morda;
@@ -12,7 +13,7 @@ ZipFile::ZipFile(ting::Ptr<ting::fs::File> zipFile, const std::string& path) :
 		ting::fs::File(path),
 		zipFile(zipFile)
 {
-	this->handle = unzOpen(this->zipFilePath.c_str());
+	this->handle = unzOpen(this->zipFile->Path().c_str());
 
 	if(!this->handle){
 		throw File::Exc("ZipFile::OpenZipFile(): opening zip file failed");
@@ -37,7 +38,9 @@ void ZipFile::OpenInternal(E_Mode mode) {
 	}
 
 	if(unzLocateFile(this->handle, this->Path().c_str(), 0) != UNZ_OK){
-		throw File::Exc("file not found");
+		std::stringstream ss;
+		ss << "ZipFile::OpenInternal(): file not found: " << this->Path();
+		throw File::Exc(ss.str());
 	}
 
 	{
@@ -53,7 +56,7 @@ void ZipFile::OpenInternal(E_Mode mode) {
 	}
 }
 
-void ZipFile::CloseInternal() {
+void ZipFile::CloseInternal()throw(){
 	if(unzCloseCurrentFile(this->handle) == UNZ_CRCERROR){
 		TRACE(<< "[WARNING] ZipFile::Close(): CRC is not good" << std::endl)
 		ASSERT(false)
@@ -88,3 +91,91 @@ bool ZipFile::Exists() const {
 }
 
 
+
+ting::Array<std::string> ZipFile::ListDirContents(size_t maxEntries){
+	if(!this->IsDir()){
+		throw File::Exc("ZipFile::ListDirContents(): this is not a directory");
+	}
+
+	//if path refers to directory then there should be no files opened
+	ASSERT(!this->IsOpened())
+
+	if(!this->handle){
+		throw File::Exc("ZipFile::ListDirContents(): zip file is not opened");
+	}
+
+	std::vector<std::string> files;
+
+	//for every file, check if it is in the current directory
+	{
+		//move to first file
+		int ret = unzGoToFirstFile(this->handle);
+
+		if(ret != UNZ_OK){
+			throw File::Exc("ZipFile::ListDirContents(): unzGoToFirstFile() failed.");
+		}
+
+		do{
+			ting::StaticBuffer<char, 255> fileNameBuf;
+
+			if(unzGetCurrentFileInfo(
+					this->handle,
+					NULL,
+					fileNameBuf.Begin(),
+					fileNameBuf.Size(),
+					NULL,
+					0,
+					NULL,
+					0
+				) != UNZ_OK)
+			{
+				throw File::Exc("ZipFile::ListDirContents(): unzGetCurrentFileInfo() failed.");
+			}
+
+			fileNameBuf[fileNameBuf.Size() - 1] = 0;//null-terminate, just to be on a safe side
+
+			std::string fn(fileNameBuf.Begin());//filename
+
+			if(fn.size() <= this->Path().size()){
+				continue;
+			}
+
+			//check if full file path starts with the this->Path() string
+			if(fn.compare(0, this->Path().size(), this->Path()) != 0){
+				continue;
+			}
+
+			ASSERT(fn.size() > this->Path().size())
+			std::string subfn(fn, this->Path().size(), fn.size() - this->Path().size());//subfilename
+
+			size_t slashPos = subfn.find_first_of('/');
+
+			//check if file is listed
+			if(slashPos == std::string::npos){
+				files.push_back(subfn);
+				continue;
+			}
+
+			//if we get here then we need to add a directory
+			ASSERT(subfn.size() >= slashPos + 1)
+			files.push_back(
+					std::string(subfn, 0, slashPos + 1)
+				);
+
+			if(files.size() == maxEntries){
+				break;
+			}
+		}while((ret = unzGoToNextFile(this->handle)) == UNZ_OK);
+
+		if(ret != UNZ_END_OF_LIST_OF_FILE && ret != UNZ_OK){
+			throw File::Exc("ZipFile::ListDirContents(): unzGoToNextFile() failed.");
+		}
+	}
+
+	ting::Array<std::string> filesArray(files.size());
+	for(unsigned i = 0; i < files.size(); ++i){
+		filesArray[i] = files[i];
+	}
+
+	return filesArray;
+}
--- src/morda/util/ZipFile.hpp
+++ src/morda/util/ZipFile.hpp
@@ -49,99 +49,13 @@ public:
 
 
 	void OpenInternal(E_Mode mode) OVERRIDE;
-	void CloseInternal() OVERRIDE;
+	void CloseInternal()throw() OVERRIDE;
 	size_t ReadInternal(const ting::Buffer<ting::u8>& buf) OVERRIDE;
 	bool Exists() const OVERRIDE;
-
-
-
-
-	virtual ting::Array<std::string> ListDirContents(size_t maxEntries = 0) OVERRIDE{
-		if(!this->IsDir()){
-			throw File::Exc("ZipFile::ListDirContents(): this is not a directory");
-		}
-
-		//if path refers to directory then there should be no files opened
-		ASSERT(!this->IsOpened())
-
-		if(!this->handle){
-			throw File::Exc("ZipFile::ListDirContents(): zip file is not opened");
-		}
-
-		std::vector<std::string> files;
-
-		//for every file, check if it is in the current directory
-		{
-			//move to first file
-			int ret = unzGoToFirstFile(this->handle);
-
-			if(ret != UNZ_OK){
-				throw File::Exc("ZipFile::ListDirContents(): unzGoToFirstFile() failed.");
-			}
-
-			do{
-				ting::StaticBuffer<char, 255> fileNameBuf;
-
-				if(unzGetCurrentFileInfo(
-						this->handle,
-						NULL,
-						fileNameBuf.Begin(),
-						fileNameBuf.Size(),
-						NULL,
-						0,
-						NULL,
-						0
-					) != UNZ_OK)
-				{
-					throw File::Exc("ZipFile::ListDirContents(): unzGetCurrentFileInfo() failed.");
-				}
-
-				fileNameBuf[fileNameBuf.Size() - 1] = 0;//null-terminate, just to be on a safe side
-
-				std::string fn(fileNameBuf.Begin());//filename
-
-				if(fn.size() <= this->Path().size()){
-					continue;
-				}
-
-				//check if full file path starts with the this->Path() string
-				if(fn.compare(0, this->Path().size(), this->Path()) != 0){
-					continue;
-				}
-
-				ASSERT(fn.size() > this->Path().size())
-				std::string subfn(fn, this->Path().size(), fn.size() - this->Path().size());//subfilename
-
-				size_t slashPos = subfn.find_first_of('/');
-
-				//check if file is listed
-				if(slashPos == std::string::npos){
-					files.push_back(subfn);
-					continue;
-				}
-
-				//if we get here then we need to add a directory
-				ASSERT(subfn.size() >= slashPos + 1)
-				files.push_back(
-						std::string(subfn, 0, slashPos + 1)
-					);
-				
-				if(files.size() == maxEntries){
-					break;
-				}
-			}while((ret = unzGoToNextFile(this->handle)) == UNZ_OK);
-
-			if(ret != UNZ_END_OF_LIST_OF_FILE && ret != UNZ_OK){
-				throw File::Exc("ZipFile::ListDirContents(): unzGoToNextFile() failed.");
-			}
-		}
-
-		ting::Array<std::string> filesArray(files.size());
-		for(unsigned i = 0; i < files.size(); ++i){
-			filesArray[i] = files[i];
-		}
-
-		return filesArray;
+	ting::Array<std::string> ListDirContents(size_t maxEntries = 0) OVERRIDE;
+	
+	static ting::Ptr<ZipFile> New(ting::Ptr<ting::fs::File> zipFile, const std::string& path = std::string()){
+		return ting::Ptr<ZipFile>(new ZipFile(zipFile, path));
 	}
 };
 
