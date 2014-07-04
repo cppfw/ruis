63937ecb921d4b29edc8b7c09f1af29706c53e7e
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2014-07-04 11:05:58 +0000

    stuff

--- src/morda/fonts/Font.cpp
+++ src/morda/fonts/Font.cpp
@@ -7,43 +7,19 @@ using namespace morda;
 
 float Font::RenderString(const morda::Matr4f& matrix, ting::utf8::Iterator str)const{
 	ting::StaticBuffer<ting::u32, 4096> buf;
-
-	ting::u32* p = buf.Begin();
-	for(; p != buf.End() && str.IsNotEnd(); ++p, ++str){
-		*p = str.Char();
-	}
-		
-	ASSERT(buf.Overlaps(p) || p == buf.End())
-	
-	return this->RenderStringInternal(matrix, ting::Buffer<const ting::u32>(buf.Begin(), p - buf.Begin()));
+	return this->RenderStringInternal(matrix, ting::Buffer<const ting::u32>(buf.Begin(), ting::utf8::FillBuffer(buf, str)));
 }
 
 
 
 float Font::StringAdvance(ting::utf8::Iterator str)const{
 	ting::StaticBuffer<ting::u32, 4096> buf;
-
-	ting::u32* p = buf.Begin();
-	for(; p != buf.End() && str.IsNotEnd(); ++p, ++str){
-		*p = str.Char();
-	}
-		
-	ASSERT(buf.Overlaps(p) || p == buf.End())
-	
-	return this->StringAdvanceInternal(ting::Buffer<const ting::u32>(buf.Begin(), p - buf.Begin()));
+	return this->StringAdvanceInternal(ting::Buffer<const ting::u32>(buf.Begin(), ting::utf8::FillBuffer(buf, str)));
 }
 
 
 
 morda::Rect2f Font::StringBoundingBox(ting::utf8::Iterator str)const{
 	ting::StaticBuffer<ting::u32, 4096> buf;
-
-	ting::u32* p = buf.Begin();
-	for(; p != buf.End() && str.IsNotEnd(); ++p, ++str){
-		*p = str.Char();
-	}
-		
-	ASSERT(buf.Overlaps(p) || p == buf.End())
-	
-	return this->StringBoundingBoxInternal(ting::Buffer<const ting::u32>(buf.Begin(), p - buf.Begin()));
+	return this->StringBoundingBoxInternal(ting::Buffer<const ting::u32>(buf.Begin(), ting::utf8::FillBuffer(buf, str)));
 }
