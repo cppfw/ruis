de966e0646bdd8058e4fcf35155bba5e9de7c7ce
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2014-07-04 10:51:54 +0000

    stuff

--- trunk/src/morda/fonts/Font.cpp
+++ trunk/src/morda/fonts/Font.cpp
@@ -6,7 +6,7 @@ using namespace morda;
 
 
 float Font::RenderString(const morda::Matr4f& matrix, ting::utf8::Iterator str)const{
-	ting::StaticBuffer<ting::u32, 1024> buf;
+	ting::StaticBuffer<ting::u32, 4096> buf;
 
 	ting::u32* p = buf.Begin();
 	for(; p != buf.End() && str.IsNotEnd(); ++p, ++str){
@@ -15,26 +15,13 @@ float Font::RenderString(const morda::Matr4f& matrix, ting::utf8::Iterator str)c
 		
 	ASSERT(buf.Overlaps(p) || p == buf.End())
 	
-	float advance = this->RenderStringInternal(matrix, ting::Buffer<const ting::u32>(buf.Begin(), p - buf.Begin()));
-	
-	while(str.IsNotEnd()){
-		morda::Matr4f matr(matrix);
-		matr.Translate(advance, 0);
-		
-		p = buf.Begin();
-		for(; p != buf.End() && str.IsNotEnd(); ++p, ++str){
-			*p = str.Char();
-		}
-		advance += this->RenderStringInternal(matr, ting::Buffer<const ting::u32>(buf.Begin(), p - buf.Begin()));
-	}
-	
-	return advance;
+	return this->RenderStringInternal(matrix, ting::Buffer<const ting::u32>(buf.Begin(), p - buf.Begin()));
 }
 
 
 
 float Font::StringAdvance(ting::utf8::Iterator str)const{
-	ting::StaticBuffer<ting::u32, 1024> buf;
+	ting::StaticBuffer<ting::u32, 4096> buf;
 
 	ting::u32* p = buf.Begin();
 	for(; p != buf.End() && str.IsNotEnd(); ++p, ++str){
@@ -43,17 +30,7 @@ float Font::StringAdvance(ting::utf8::Iterator str)const{
 		
 	ASSERT(buf.Overlaps(p) || p == buf.End())
 	
-	float advance = this->StringAdvanceInternal(ting::Buffer<const ting::u32>(buf.Begin(), p - buf.Begin()));
-	
-	while(str.IsNotEnd()){		
-		p = buf.Begin();
-		for(; p != buf.End() && str.IsNotEnd(); ++p, ++str){
-			*p = str.Char();
-		}
-		advance += this->StringAdvanceInternal(ting::Buffer<const ting::u32>(buf.Begin(), p - buf.Begin()));
-	}
-	
-	return advance;
+	return this->StringAdvanceInternal(ting::Buffer<const ting::u32>(buf.Begin(), p - buf.Begin()));
 }
 
 
