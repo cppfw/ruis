d2a6ab9fe444368c3e133822cffac11ba52bd4d4
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2014-07-14 05:41:12 +0000

    c++11

--- trunk/android/tests/jni/Application.mk
+++ trunk/android/tests/jni/Application.mk
@@ -1,3 +1,5 @@
+NDK_TOOLCHAIN_VERSION := 4.8
+
 APP_PLATFORM := android-9
 
 #we want to use exceptions
@@ -5,6 +7,8 @@ APP_CPPFLAGS += -fexceptions
 
 #we want RTTI
 APP_CPPFLAGS += -frtti
+
+APP_CPPFLAGS += -std=c++11
 
 #we want to use stl
 APP_STL := gnustl_shared
