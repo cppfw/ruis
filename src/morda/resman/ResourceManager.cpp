313b1a3567775951e10d10e821e3b338992cfcdc
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2014-07-25 13:19:26 +0000

    stuff

--- src/morda/resman/ResourceManager.cpp
+++ src/morda/resman/ResourceManager.cpp
@@ -65,22 +65,19 @@ ResourceManager::FindInScriptRet ResourceManager::FindResourceInScript(const std
 void ResourceManager::AddResource(const ting::Ref<Resource>& res, const stob::Node& node){
 	ASSERT(res)
 
+	ASSERT(this->resMap.find(node.Value()) == this->resMap.end())
+	
 	//add the resource to the resources map of ResMan
-	std::pair<T_ResMap::iterator, bool> pr = this->resMap->rm.insert(
+	this->resMap.insert(
 			std::pair<const char*, ting::WeakRef<Resource> >(
 					node.Value(),
 					res.GetWeakRef()
 				)
 		);
 
-	ASSERT(pr.second == true) //make sure that the new element was added but not the old one rewritten
-
 //#ifdef DEBUG
 //	for(T_ResMap::iterator i = this->resMap->rm.begin(); i != this->resMap->rm.end(); ++i){
 //		TRACE(<< "\t" << *(*i).first << std::endl)
 //	}
 //#endif
-
-	res->it = pr.first;
-	res->rm = this->resMap; //save weak reference to resource map
 }
