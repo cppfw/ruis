25e40e69ec78e78312f28b1f186b0273d431e3e0
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2014-09-27 20:49:56 +0000

    stuff

--- src/morda/widgets/LinearWidget.cpp
+++ src/morda/widgets/LinearWidget.cpp
@@ -15,10 +15,6 @@ LinearWidget::LinearWidget(const stob::Node* desc) :
 	if(const stob::Node* n = desc->GetProperty("vertical")){
 		this->isVertical = n->AsBool();
 	}
-	
-	if(const stob::Node* n = desc->GetProperty("reverse")){
-		this->isReverse = n->AsBool();
-	}
 }
 
 
