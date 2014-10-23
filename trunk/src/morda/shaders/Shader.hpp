afe5c827093e72938447549c099329e5398d86d1
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2014-10-23 10:58:19 +0000

    stuff

--- trunk/src/morda/shaders/Shader.hpp
+++ trunk/src/morda/shaders/Shader.hpp
@@ -150,7 +150,31 @@ public:
 	}
 	
 	static void AssertOpenGLNoError(){
-		ASSERT(glGetError() == GL_NO_ERROR)
+#ifdef DEBUG
+		GLenum error = glGetError();
+		switch(error){
+			case GL_NO_ERROR:
+				return;
+			case GL_INVALID_ENUM:
+				ASSERT_INFO(false, "OpenGL error: GL_INVALID_ENUM")
+				break;
+			case GL_INVALID_VALUE:
+				ASSERT_INFO(false, "OpenGL error: GL_INVALID_VALUE")
+				break;
+			case GL_INVALID_OPERATION:
+				ASSERT_INFO(false, "OpenGL error: GL_INVALID_OPERATION")
+				break;
+			case GL_INVALID_FRAMEBUFFER_OPERATION:
+				ASSERT_INFO(false, "OpenGL error: GL_INVALID_FRAMEBUFFER_OPERATION")
+				break;
+			case GL_OUT_OF_MEMORY:
+				ASSERT_INFO(false, "OpenGL error: GL_OUT_OF_MEMORY")
+				break;
+			default:
+				ASSERT_INFO(false, "Unknown OpenGL error, code = " << int(error))
+				break;
+		}
+#endif
 	}
 };//~class Shader
 
