54a26ee3abed54809849b036009bc4f4de503fe7
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2014-10-24 11:25:57 +0000

    stuff

--- trunk/src/morda/widgets/BlurGlass.cpp
+++ trunk/src/morda/widgets/BlurGlass.cpp
@@ -20,17 +20,6 @@ void BlurGlass::Render(const morda::Matr4r& matrix)const{
 	GLTexture texture(this->Rect().d.ConvertTo<unsigned>(), 4, GL_NEAREST, GL_NEAREST);
 	//texture is bound right after creation
 	
-	glCopyTexSubImage2D(
-			GL_TEXTURE_2D,
-			0, //level
-			0, //xoffset
-			0, //yoffset
-			viewPortRect.p.x,
-			viewPortRect.p.y,
-			viewPortRect.d.x,
-			viewPortRect.d.y
-		);
-	
 	morda::Matr4r matr(matrix);
 	matr.Scale(this->Rect().d);
 
@@ -40,5 +29,18 @@ void BlurGlass::Render(const morda::Matr4r& matrix)const{
 	
 	s.SetTextureStep(Vec2f(1).CompDiv(viewPortRect.d.ConvertTo<float>()));
 	
-	s.Render(morda::PosShader::quad01Fan, s.quadFanTexCoords);
+	for(unsigned i = 0; i != 1; ++i){
+		glCopyTexSubImage2D(
+				GL_TEXTURE_2D,
+				0, //level
+				0, //xoffset
+				0, //yoffset
+				viewPortRect.p.x,
+				viewPortRect.p.y,
+				viewPortRect.d.x,
+				viewPortRect.d.y
+			);
+		
+		s.Render(morda::PosShader::quad01Fan, s.quadFanTexCoords);
+	}
 }
