a94d17ea569ff40fc98acf361baedb20b8c39acf
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2014-11-05 14:24:49 +0000

    refactoring

--- src/morda/resources/ResFont.cpp
+++ src/morda/resources/ResFont.cpp
@@ -18,29 +18,17 @@ ResFont::ResFont(const ting::fs::File& fi, const ting::Buffer<std::uint32_t> cha
 
 //static
 std::shared_ptr<ResFont> ResFont::Load(const stob::Node& chain, const ting::fs::File& fi){
-	//get font filename
-	const stob::Node* fileProp = chain.ChildOfThisOrNext("file");
-	if(!fileProp){
-		throw morda::Exc("ResFont::Load(): no 'file' property in resource description");
-	}
-
 	//read chars attribute
 	std::vector<std::uint32_t> wideChars;
-	{
-		const stob::Node* charsProp = chain.ChildOfThisOrNext("chars");
-		if(!charsProp){
-			throw morda::Exc("ResFont::Load(): no 'chars' property in resource description");
-		}
-		
-		for(ting::utf8::Iterator i(charsProp->Value()); i.IsNotEnd(); ++i){
-			wideChars.push_back(i.Char());
-		}
+	for(ting::utf8::Iterator i(chain.side("chars").up().Value()); i.IsNotEnd(); ++i){
+		wideChars.push_back(i.Char());
 	}
+
 	ASSERT(wideChars.size() > 0)
 
 	//read size attribute
 	unsigned size;
-	if(const stob::Node* sizeProp = chain.ChildOfThisOrNext("size")){
+	if(auto sizeProp = chain.ChildOfThisOrNext("size")){
 		size = unsigned(morda::DimValueFromSTOB(*sizeProp));
 	}else{
 		size = 13;
@@ -48,16 +36,13 @@ std::shared_ptr<ResFont> ResFont::Load(const stob::Node& chain, const ting::fs::
 	
 	//read outline attribute
 	unsigned outline;
-	{
-		const stob::Node* outlineProp = chain.ChildOfThisOrNext("outline");
-		if(!outlineProp){
-			outline = 0;
-		}else{
-			outline = unsigned(outlineProp->AsUint32());
-		}
+	if(auto outlineProp = chain.ChildOfThisOrNext("outline")){
+		outline = unsigned(outlineProp->AsUint32());
+	}else{
+		outline = 0;
 	}
 
-	fi.SetPath(fileProp->Value());
+	fi.SetPath(chain.side("file").up().Value());
 
 	return std::move(ting::New<ResFont>(fi, ting::Buffer<std::uint32_t>(&(*wideChars.begin()), wideChars.size()), size, outline));
 }
