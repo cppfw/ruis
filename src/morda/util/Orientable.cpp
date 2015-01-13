28de87c59b4c5ab7936494b380ed890abad40aaf
igagis <igagis@a9c5d648-722e-0610-0baa-622df23d3171>
2015-01-13 12:52:24 +0000

    stuff

---
+++ src/morda/util/Orientable.cpp
@@ -0,0 +1,19 @@
+#include "Orientable.hpp"
+#include "../util/util.hpp"
+
+
+using namespace morda;
+
+
+
+Orientable::Orientable(const stob::Node* chain) :
+		Widget(chain)
+{
+	if(const stob::Node* n = GetProperty(chain, "vertical")){
+		this->isVertical = n->AsBool();
+	}else{
+		this->isVertical = false;
+	}
+}
+
+
