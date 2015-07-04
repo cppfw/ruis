0170cceb32ee005e35f79c93303398b3efc4a269
Ivan Gagis <igagis@gmail.com>
2015-07-04 16:22:04 +0300

    stuff

--- src/morda/render/Render_DirectX.cpp
+++ src/morda/render/Render_DirectX.cpp
@@ -1,4 +1,4 @@
-
+#include <d3d11.h>
 
 #include <memory>
 
@@ -8,6 +8,8 @@
 #include <ting/Buffer.hpp>
 
 #include "../Exc.hpp"
+
+#include "../App.hpp"
 
 using namespace morda;
 
@@ -93,7 +95,55 @@ void Render::setClearColor(Vec4f c){
 	//TODO:
 }
 
-Render::Render(){
+namespace {
+
+struct Direct3DContext : public ting::Void {
+	IDXGISwapChain *swapchain;
+	ID3D11Device *dev;
+	ID3D11DeviceContext *ctx;
+
+	Direct3DContext(HWND hwnd) {
+		// create a struct to hold information about the swap chain
+		DXGI_SWAP_CHAIN_DESC scd;
+
+		// clear out the struct for use
+		ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+		// fill the swap chain description struct
+		scd.BufferCount = 1;                                    // one back buffer
+		scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
+		scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+		scd.OutputWindow = hwnd;                                // the window to be used
+		scd.SampleDesc.Count = 4;                               // how many multisamples
+		scd.Windowed = TRUE;                                    // windowed/full-screen mode
+
+																// create a device, device context and swap chain using the information in the scd struct
+		D3D11CreateDeviceAndSwapChain(NULL,
+			D3D_DRIVER_TYPE_HARDWARE,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			D3D11_SDK_VERSION,
+			&scd,
+			&this->swapchain,
+			&this->dev,
+			NULL,
+			&this->ctx);
+	}
+
+	~Direct3DContext()noexcept {
+		swapchain->Release();
+		dev->Release();
+		ctx->Release();
+	}
+};
+
+}//~namespace
+
+Render::Render() :
+		pimpl(new Direct3DContext(morda::App::Inst().window.hwnd))
+{
 	//TODO:
 }
 
