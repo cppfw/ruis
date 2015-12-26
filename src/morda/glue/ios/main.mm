b8a8103bb359eca9b67d4268789d428c95932a2e
Ivan Gagis <igagis@gmail.com>
2015-12-26 21:20:04 +0200

    stuff

--- src/morda/glue/ios/main.mm
+++ src/morda/glue/ios/main.mm
@@ -6,13 +6,80 @@
 //  Copyright (c) 2015 org.igagis. All rights reserved.
 //
 
+#include "../../App.hpp"
+
 #import <UIKit/UIKit.h>
 
-#import "AppDelegate.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+	// Override point for customization after application launch.
+	return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
+
+
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
+}
+
+
+
+void morda::App::postToUiThread_ts(std::function<void()>&& f){
+	/*
+	NSEvent* e = [NSEvent
+				  otherEventWithType: NSApplicationDefined
+				  location: NSMakePoint(0, 0)
+				  modifierFlags:0
+				  timestamp:0
+				  windowNumber:0
+				  context: nil
+				  subtype: 0
+				  data1: reinterpret_cast<NSInteger>(new std::function<void()>(std::move(f)))
+				  data2: 0
+				  ];
+	
+	NSApplication *applicationObject = (NSApplication*)this->applicationObject.id;
+	[applicationObject postEvent:e atStart:NO];
+	*/
 }
