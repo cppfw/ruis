6864787c5646ed02305ca535cf0c7b679b16009c
Ivan Gagis <igagis@gmail.com>
2016-05-10 14:39:34 +0300

    stuff

--- src/morda/resources/ResGradient.cpp
+++ src/morda/resources/ResGradient.cpp
@@ -9,4 +9,24 @@
 
 
 
+using namespace morda;
+
+
+
+ResGradient::ResGradient(const std::vector<std::tuple<real,std::uint32_t> >& stops, bool vertical){
+	//TODO:
+}
+
+
+
+std::shared_ptr<ResGradient> ResGradient::load(const stob::Node& chain, const papki::File& fi) {
+	//TODO:
+	
+	return nullptr;
+}
+
+
+void ResGradient::render(const Matr4r& matrix, ClrPosShader& s) const {
+	//TODO:
+}
 
--- src/morda/resources/ResGradient.hpp
+++ src/morda/resources/ResGradient.hpp
@@ -9,28 +9,30 @@
 
 #include "../ResourceManager.hpp"
 
-#include "../shaders/ColorPosShader.hpp"
+#include "../shaders/ClrPosShader.hpp"
 
 #include "../config.hpp"
+
+#include <kolme/Vector2.hpp>
 
 namespace morda{
 
 class ResGradient : public Resource{
+	friend class ResourceManager;
 	
-	std::vector<real> stops;
+	std::vector<kolme::Vec2f> vertices;
+	std::vector<std::uint32_t> colors;
+	
 public:
-	ResGradient(std::vector<real>&& stops) :
-			stops(stops)
-	{}
+	ResGradient(const std::vector<std::tuple<real, std::uint32_t>>& stops, bool vertical);
 	
 	ResGradient(const ResGradient&) = delete;
 	ResGradient& operator=(const ResGradient&) = delete;
 	
-	
-	void render(const Matr4r& matrix)const;
+	void render(const Matr4r& matrix, ClrPosShader& s)const;
 	
 private:
-
+	static std::shared_ptr<ResGradient> load(const stob::Node& chain, const papki::File& fi);
 };
 
 }
