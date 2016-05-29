7df15a2fce8066ee0d9c70410849237feb92af45
Ivan Gagis <igagis@gmail.com>
2016-05-29 21:22:06 +0300

    stuff

--- src/morda/util/Sides.hpp
+++ src/morda/util/Sides.hpp
@@ -7,35 +7,34 @@
 
 #pragma once
 
-#include <kolme/Rectangle.hpp>
+#include <array>
 
 namespace morda{
 
-template <class T> class Sides : private kolme::Rectangle<T>{
+template <class T> class Sides : private std::array<T, 4>{
 	
 public:
 	Sides(){}
 	
 	Sides(T l, T t, T r, T b) :
-			kolme::Rectangle<T>(l, b, r, t)
+			std::array<T, 4>({{l, t, r, b}})
 	{}
 	
 	T& left()noexcept{
-		return this->p.x;
-	}
-	
-	T& right()noexcept{
-		return this->d.x;
-	}
-	
-	T& bottom()noexcept{
-		return this->p.y;
+		return this->operator[](0);
 	}
 	
 	T& top()noexcept{
-		return this->d.y;
+		return this->operator[](1);
 	}
 	
+	T& right()noexcept{
+		return this->operator[](2);
+	}
+	
+	T& bottom()noexcept{
+		return this->operator[](3);
+	}
 };
 
 }
