c252258a92a28762ff5fd8eb2f24b0aebe693dae
Ivan Gagis <igagis@gmail.com>
2019-01-07 14:27:24 +0300

    updating msvs project to v141 tools

--- nuget/build_nuget.ps1
+++ nuget/build_nuget.ps1
@@ -18,21 +18,23 @@ Get-ChildItem "." -Filter *.in | Foreach-Object{
 }
 
 
-"%VS140COMNTOOLS%\VsMSBuildCmd.bat"
-msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=Debug /p:Platform=x86 /v:minimal /nologo
+"%VS141COMNTOOLS%\VsMSBuildCmd.bat"
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Debug /p:Platform=x86 /v:minimal /nologo
 If(!$?){exit 1}
-msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=Release /p:Platform=x86 /v:minimal /nologo
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Release /p:Platform=x86 /v:minimal /nologo
 If(!$?){exit 1}
-msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=Debug /p:Platform=x64 /v:minimal /nologo
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Debug /p:Platform=x64 /v:minimal /nologo
 If(!$?){exit 1}
-msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=Release /p:Platform=x64 /v:minimal /nologo
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Release /p:Platform=x64 /v:minimal /nologo
 If(!$?){exit 1}
-
-$autopkgFiles = @(gci *.autopkg)
-
-ForEach ($autopkgFile in $autopkgFiles) {
-    Write-NuGetPackage $autopkgFile
-    If(!$?){exit 1}
-}
-
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Debug /p:Platform=x86 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x86 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Debug /p:Platform=x64 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x64 /v:minimal /nologo
+If(!$?){exit 1}
+Write-NuGetPackage nuget.autopkg
+If(!$?){exit 1}
 Pop-Location
