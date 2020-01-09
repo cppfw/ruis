396c751e394cd334f25059dbdae3b45e30e85740
Ivan Gagis <igagis@gmail.com>
2020-01-09 17:42:31 +0200

    stuff

--- src/morda/widgets/label/Color.hpp
+++ src/morda/widgets/label/Color.hpp
@@ -13,7 +13,8 @@ namespace morda{
  */
 class Color : public ColorWidget{
 public:
-	Color(const stob::Node* chain);
+	Color(const puu::forest& desc);
+	Color(const stob::Node* chain) : Color(stob_to_puu(chain)){}
 
 	Color(const Color&) = delete;
 	Color& operator=(const Color&) = delete;
