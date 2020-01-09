d456aa497165544794174c3029e3944206dbfef1
Ivan Gagis <igagis@gmail.com>
2020-01-09 14:30:37 +0200

    Fraction widget moved to puu

--- src/morda/widgets/base/FractionWidget.hpp
+++ src/morda/widgets/base/FractionWidget.hpp
@@ -6,8 +6,12 @@ namespace morda{
 class FractionWidget : public virtual Widget{
 	float fraction_v = 0; //Current position from 0 to 1
 protected:
+	FractionWidget(const puu::trees& desc) :
+		widget(desc)
+	{}
+
 	FractionWidget(const stob::Node* chain) :
-			Widget(chain)
+			FractionWidget(stob_to_puu(chain))
 	{}
 
 	virtual void onFractionChange();
