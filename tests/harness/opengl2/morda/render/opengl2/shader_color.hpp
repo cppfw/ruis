e6b4df55bae8c392a6647f35bc837502ab99b79a
Ivan Gagis <igagis@gmail.com>
2020-06-20 03:43:24 +0300

    refactoring

--- tests/harness/opengl2/morda/render/opengl2/shader_color.hpp
+++ tests/harness/opengl2/morda/render/opengl2/shader_color.hpp
@@ -6,16 +6,16 @@
 
 namespace morda{ namespace render_opengl2{
 
-class OpenGL2ShaderColor :
+class shader_color :
 		public morda::coloring_shader,
 		public OpenGL2ShaderBase
 {
 	GLint colorUniform;
 public:
-	OpenGL2ShaderColor();
+	shader_color();
 	
-	OpenGL2ShaderColor(const OpenGL2ShaderColor&) = delete;
-	OpenGL2ShaderColor& operator=(const OpenGL2ShaderColor&) = delete;
+	shader_color(const shader_color&) = delete;
+	shader_color& operator=(const shader_color&) = delete;
 	
 
 	void render(const r4::mat4f& m, const morda::vertex_array& va, r4::vec4f color)const override;
