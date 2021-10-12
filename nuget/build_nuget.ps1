e62fab0ccc36fe299c1a58cc98fe9765ff30e09d
Ivan Gagis <igagis@gmail.com>
2021-10-12 15:58:19 +0300

    msvs: run unit tests

--- nuget/build_nuget.ps1
+++ nuget/build_nuget.ps1
@@ -37,22 +37,26 @@ Get-ChildItem "." -Filter *.in | Foreach-Object{
 # msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x64 /v:minimal /nologo
 # If(!$?){exit 1}
 
-msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MD /p:Platform=x86 /v:minimal /nologo
-If(!$?){exit 1}
-msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MD /p:Platform=x86 /v:minimal /nologo
-If(!$?){exit 1}
-msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MD /p:Platform=x64 /v:minimal /nologo
-If(!$?){exit 1}
-msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MD /p:Platform=x64 /v:minimal /nologo
-If(!$?){exit 1}
-# msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MT /p:Platform=x86 /v:minimal /nologo
-# If(!$?){exit 1}
-# msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MT /p:Platform=x86 /v:minimal /nologo
-# If(!$?){exit 1}
-# msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MT /p:Platform=x64 /v:minimal /nologo
-# If(!$?){exit 1}
-# msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MT /p:Platform=x64 /v:minimal /nologo
-# If(!$?){exit 1}
+msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MD /p:Platform=x86 /v:minimal /nologo && If(!$?){exit 1}
+msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MD /p:Platform=x86 /v:minimal /nologo && If(!$?){exit 1}
+msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MD /p:Platform=x64 /v:minimal /nologo && If(!$?){exit 1}
+msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MD /p:Platform=x64 /v:minimal /nologo && If(!$?){exit 1}
+# msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MT /p:Platform=x86 /v:minimal /nologo && If(!$?){exit 1}
+# msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MT /p:Platform=x86 /v:minimal /nologo && If(!$?){exit 1}
+# msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MT /p:Platform=x64 /v:minimal /nologo && If(!$?){exit 1}
+# msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MT /p:Platform=x64 /v:minimal /nologo && If(!$?){exit 1}
+
+Write-Host "running tests..."
+# run release tests first
+../msvs_solution/v142_Release_MD/unit_tests.exe     --jobs=2 --junit-out=junit_x86_v142_release_md.xml; If(!$?){exit 1}
+../msvs_solution/x64/v142_Release_MD/unit_tests.exe --jobs=2 --junit-out=junit_x64_v142_release_md.xml; If(!$?){exit 1}
+../msvs_solution/v142_Debug_MD/unit_tests.exe       --jobs=2 --junit-out=junit_x86_v142_debug_md.xml; If(!$?){exit 1}
+../msvs_solution/x64/v142_Debug_MD/unit_tests.exe   --jobs=2 --junit-out=junit_x64_v142_debug_md.xml; If(!$?){exit 1}
+# ../msvs_solution/v142_Release_MT/unit_tests.exe     --jobs=2 --junit-out=junit_x86_v142_release_mt.xml; If(!$?){exit 1}
+# ../msvs_solution/x64/v142_Release_MT/unit_tests.exe --jobs=2 --junit-out=junit_x64_v142_release_mt.xml; If(!$?){exit 1}
+# ../msvs_solution/v142_Debug_MT/unit_tests.exe       --jobs=2 --junit-out=junit_x86_v142_debug_mt.xml; If(!$?){exit 1}
+# ../msvs_solution/x64/v142_Debug_MT/unit_tests.exe   --jobs=2 --junit-out=junit_x64_v142_debug_mt.xml; If(!$?){exit 1}
+
 
 Write-NuGetPackage nuget.autopkg
 If(!$?){exit 1}
