7b734cf5c216b5fd7f261e2a65fc9782e45b35ed
Ivan Gagis <ivan.gagis@ge.com>
2023-06-30 13:04:36 +0300

    fix lint

--- tests/harness/util/dummy_context.cpp
+++ tests/harness/util/dummy_context.cpp
@@ -6,7 +6,7 @@ utki::shared_ref<morda::context> make_dummy_context(){
     return utki::make_shared<morda::context>(
             utki::make_shared<fake_renderer>(),
             utki::make_shared<morda::updater>(),
-            [](std::function<void()>&&){},
+            [](std::function<void()>){},
             [](morda::mouse_cursor){},
             morda::real(0),
             morda::real(0)
