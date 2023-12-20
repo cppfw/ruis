61b614d61a1faa58948ac120972dd36b63dc1923
Ivan Gagis <ivan.gagis@ge.com>
2023-12-20 13:46:27 +0200

    stuff

--- src/morda/fonts/font_factory.hpp
+++ src/morda/fonts/font_factory.hpp
@@ -1,12 +1,34 @@
+/*
+morda - GUI framework
+
+Copyright (C) 2012-2023  Ivan Gagis <igagis@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* ================ LICENSE END ================ */
+
 #pragma once
 
 #include "font.hpp"
 
-namespace feis{
+namespace morda {
 
-class font_factory{
+class font_factory
+{
 public:
-    virtual std::unique_ptr<font> create(size_t size) = 0;
+	virtual std::unique_ptr<font> create(size_t size) = 0;
 };
 
-}
+} // namespace morda
