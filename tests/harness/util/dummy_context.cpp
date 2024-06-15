ea3fbd5b5a7d43a71e7c8aab9c72e50dfea57ece
Ivan Gagis <igagis@gmail.com>
2024-06-15 12:24:54 +0300

    null renderer submodule

--- tests/harness/util/dummy_context.cpp
+++ tests/harness/util/dummy_context.cpp
@@ -1,10 +1,10 @@
 #include "dummy_context.hpp"
 
-#include "null_renderer.hpp"
+#include <ruis/render/null/renderer.hpp>
 
 utki::shared_ref<ruis::context> make_dummy_context(){
     return utki::make_shared<ruis::context>(
-            utki::make_shared<null_renderer>(),
+            utki::make_shared<ruis::render::null::renderer>(),
             utki::make_shared<ruis::updater>(),
             [](std::function<void()>){},
             [](ruis::mouse_cursor){},
--- tests/harness/util/null_renderer.cpp
+++
@@ -1,3 +0,0 @@
-#include "null_renderer.hpp"
-
-
