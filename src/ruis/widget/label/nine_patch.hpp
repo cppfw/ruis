86e202ccc010796a835353b488abe9cd5bc0deff
Ivan Gagis <igagis@gmail.com>
2024-07-09 16:58:36 +0300

    add TODO:

--- src/ruis/widget/label/nine_patch.hpp
+++ src/ruis/widget/label/nine_patch.hpp
@@ -61,6 +61,7 @@ private:
 
 public:
 	struct all_parameters {
+		// TODO: add container_params
 		widget::parameters widget_params;
 		blending_widget::parameters blending_params;
 		frame_widget::parameters frame_params;
