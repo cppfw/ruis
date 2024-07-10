9e48014ba64625c084b70569ff895e6285557db4
Ivan Gagis <igagis@gmail.com>
2024-07-10 11:51:24 +0300

    fix build

--- src/ruis/widget/base/text_widget.cpp
+++ src/ruis/widget/base/text_widget.cpp
@@ -108,7 +108,7 @@ text_widget::text_widget(const utki::shared_ref<ruis::context>& c, const tml::fo
 }
 
 text_widget::text_widget(utki::shared_ref<ruis::context> context, parameters params) :
-	widget(std::move(context), widget::all_parameters{}),
+	widget(std::move(context), {}, {}),
 	params([this, &params]() {
 		auto p = std::move(params);
 		if (!p.font_face) {
