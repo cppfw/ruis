eb16d87cbf67559c4e2cc1514cdb06d73686488d
Ivan Gagis <igagis@gmail.com>
2024-08-18 01:10:04 +0300

    add timer test

--- tests/unit/src/timer.cpp
+++ tests/unit/src/timer.cpp
@@ -14,13 +14,13 @@ const tst::set set("timer", [](tst::suite& suite){
             }
         );
 
-        bool triggered = false;
+        unsigned num_triggered = 0;
         uint32_t time_elapsed_ms = 0;
 
         auto timer = utki::make_shared<ruis::timer>(
             updater,
             [&](uint32_t elapsed_ms){
-                triggered = true;
+                ++num_triggered;
                 time_elapsed_ms = elapsed_ms;
             }
         );
@@ -29,33 +29,33 @@ const tst::set set("timer", [](tst::suite& suite){
 
         updater.get().update();
 
-        tst::check(!triggered, SL);
+        tst::check_eq(num_triggered, unsigned(0), SL);
         tst::check(!timer.get().is_running(), SL);
 
         cur_ticks = 20;
 
         updater.get().update();
 
-        tst::check(!triggered, SL);
+        tst::check_eq(num_triggered, unsigned(0), SL);
         tst::check(!timer.get().is_running(), SL);
 
         timer.get().start(10);
 
         tst::check_eq(timer.get().get_timeout_ms(), uint32_t(10), SL);
 
-        tst::check(!triggered, SL);
+        tst::check_eq(num_triggered, unsigned(0), SL);
         tst::check(timer.get().is_running(), SL);
 
         cur_ticks = 28;
         updater.get().update();
 
-        tst::check(!triggered, SL);
+        tst::check_eq(num_triggered, unsigned(0), SL);
         tst::check(timer.get().is_running(), SL);
 
         cur_ticks = 30;
         updater.get().update();
 
-        tst::check(triggered, SL);
+        tst::check_eq(num_triggered, unsigned(1), SL);
         tst::check(!timer.get().is_running(), SL);
 
         tst::check_ge(time_elapsed_ms, uint32_t(10), SL);
@@ -63,7 +63,51 @@ const tst::set set("timer", [](tst::suite& suite){
 
     // TODO: add test: stopping before expired
 
-    // TODO: add test: restarting from callback
+    suite.add("restart_from_callback", [](){
+        uint32_t cur_ticks = 0;
+
+        auto updater = utki::make_shared<ruis::updater>(
+            [&](){
+                return cur_ticks;
+            }
+        );
+
+        unsigned num_triggered = 0;
+
+        utki::shared_ref<ruis::timer> timer = utki::make_shared<ruis::timer>(
+            updater,
+            [&](uint32_t elapsed_ms){
+                ++num_triggered;
+                timer.get().start(10);
+            }
+        );
+
+        tst::check_eq(num_triggered, unsigned(0), SL);
+
+        timer.get().start(10);
+
+        tst::check_eq(num_triggered, unsigned(0), SL);
+
+        cur_ticks = 9;
+        updater.get().update();
+
+        tst::check_eq(num_triggered, unsigned(0), SL);
+
+        cur_ticks = 11;
+        updater.get().update();
+
+        tst::check_eq(num_triggered, unsigned(1), SL);
+
+        cur_ticks = 22;
+        updater.get().update();
+
+        tst::check_eq(num_triggered, unsigned(2), SL);
+
+        cur_ticks = 33;
+        updater.get().update();
+
+        tst::check_eq(num_triggered, unsigned(3), SL);
+    });
 
     // TODO: add test: stopping after expired
 
