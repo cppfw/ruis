073955708e99ada00310b1cd29103e8e96e4e43b
Ivan Gagis <igagis@gmail.com>
2024-08-30 15:41:07 +0300

    stuff

--- src/ruis/layout/layout_parameters.hpp
+++ src/ruis/layout/layout_parameters.hpp
@@ -32,7 +32,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 namespace ruis {
 
 /**
- * @brief Layout parameters.
+ * @brief Widget layout parameters.
  */
 struct layout_parameters {
 	/**
