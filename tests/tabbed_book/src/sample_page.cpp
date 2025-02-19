389ac065f9648462e61507df154b355bc9de985b
Ivan Gagis <igagis@gmail.com>
2025-02-19 14:58:49 +0200

    resolve TODO

--- tests/tabbed_book/src/sample_page.cpp
+++ tests/tabbed_book/src/sample_page.cpp
@@ -3,29 +3,49 @@
 #include <ruis/widget/label/text.hpp>
 #include <ruis/widget/button/push_button.hpp>
 
-sample_page::sample_page(const utki::shared_ref<ruis::context>& c, const std::string& text) :
-        ruis::widget(c, tml::forest()),
-        ruis::page(this->context, tml::forest()),
-        // TODO: modernize
-        ruis::container(this->context, tml::read(R"qwer(
-                layout{row}
-                
-                @text{
-                        id{text}
-                }
-                @push_button{
-                        id{button}
-                        @text{
-                                text{"tear out"}
-                        }
-                }
-        )qwer"))
-{
-        this->get_widget_as<ruis::text>("text").set_text(text);
+using namespace std::string_literals;
 
-        this->get_widget_as<ruis::push_button>("button").click_handler = [this](ruis::push_button&){
-                this->context.get().post_to_ui_thread([pg = utki::make_shared_from(*this)]{
-                        pg.get().tear_out();
-                });
-        };
+sample_page::sample_page(const utki::shared_ref<ruis::context>& c, const std::string& text) :
+	ruis::widget(c, tml::forest()),
+	ruis::page(this->context, tml::forest()),
+	// clang-format off
+	ruis::container(
+		this->context,
+		{
+			.container_params{
+				.layout = ruis::layout::row
+			}
+		},
+		{
+			ruis::make::text(this->context,
+				{
+					.widget_params{
+						.id = "text"s
+					}
+				}
+			),
+			ruis::make::push_button(this->context,
+				{
+					.widget_params{
+						.id = "button"s
+					}
+				},
+				{
+					ruis::make::text(this->context,
+						{},
+						U"tear out the page"s
+					)
+				}
+			)
+		}
+	)
+	// clang-format on
+{
+	this->get_widget_as<ruis::text>("text").set_text(text);
+
+	this->get_widget_as<ruis::push_button>("button").click_handler = [this](ruis::push_button&){
+		this->context.get().post_to_ui_thread([pg = utki::make_shared_from(*this)]{
+			pg.get().tear_out();
+		});
+	};
 }
