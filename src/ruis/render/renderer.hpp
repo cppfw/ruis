5eec4ffc40a89f60d66afebb9f216ab1296d4f82
Ivan Gagis <igagis@gmail.com>
2025-03-11 01:09:16 +0200

    stuff

--- src/ruis/render/renderer.hpp
+++ src/ruis/render/renderer.hpp
@@ -31,19 +31,13 @@ public:
 	const utki::shared_ref<ruis::render::context> render_context;
 
 	struct objects {
-		const utki::shared_ref<const ruis::render::context::shaders> shaders;
-
-		const utki::shared_ref<const vertex_array> empty_vertex_array;
-
-		const utki::shared_ref<const vertex_buffer> quad_01_vbo;
-
-		const utki::shared_ref<const index_buffer> quad_indices;
-
-		const utki::shared_ref<const vertex_array> pos_quad_01_vao;
-
-		const utki::shared_ref<const vertex_array> pos_tex_quad_01_vao;
-
-		const utki::shared_ref<const texture_2d> white_texture;
+		utki::shared_ref<const ruis::render::context::shaders> shaders;
+		utki::shared_ref<const vertex_array> empty_vertex_array;
+		utki::shared_ref<const vertex_buffer> quad_01_vbo;
+		utki::shared_ref<const index_buffer> quad_indices;
+		utki::shared_ref<const vertex_array> pos_quad_01_vao;
+		utki::shared_ref<const vertex_array> pos_tex_quad_01_vao;
+		utki::shared_ref<const texture_2d> white_texture;
 	};
 
 	const utki::shared_ref<const objects> common_objects;
