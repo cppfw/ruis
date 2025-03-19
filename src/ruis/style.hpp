6b8b5d9952a028338c68902797227071908d6d41
Ivan Gagis <igagis@gmail.com>
2025-03-19 14:53:59 +0200

    stuff

--- src/ruis/style.hpp
+++ src/ruis/style.hpp
@@ -1,43 +1,50 @@
 #pragma once
 
+#include <utki/shared_ref.hpp>
+#include <tml/tree.hpp>
+
 namespace ruis {
+
+class style_value_base
+{
+	utki::shared_ref<style_sheet> owner_sheet;
+public:
+	virtual ~style_value_base() = default;
+
+	virtual void reload(const tml::forest& desc) = 0;
+};
 
 class style_sheet
 {
-public:
-};
+	std::shared_ptr<style_value_base> get(std::string_view style_name);
 
-template <typename value_type>
-class style_value
-{
-public:
-	utki::shared_ref<style_sheet> ss;
+	// TODO: weak_ptr cache and reload cached values
 
-	value_type value;
+public:
+	template <typename value_type>
+	utki::shared_ref<value_type> get(
+		std::string_view style_name, //
+		const value_type& default_value
+	)
+	{
+		// TODO:
+	}
 };
 
 template <typename value_type>
 class styled
 {
-	std::shared_ptr<style_value> value;
-	value_type cur_value;
+	std::variant<
+	utki::shared_ref<style_value>,
+	value_type> value;
 
 public:
 	styled(value_type value) :
-		cur_value(std::move(value))
+		value(std::move(value))
 	{}
 
-	const value_type& get()
+	const value_type& get()const noexcept
 	{
-		this->cur_value;
-	}
-
-	void reload()
-	{
-		if(!this->value){
-			return;
-		}
-
 		// TODO:
 	}
 };
