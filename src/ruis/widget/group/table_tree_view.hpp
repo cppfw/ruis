f9e8bcbd957e2e51718335b57c7c25d4f375beb5
Ivan Gagis <igagis@gmail.com>
2025-07-26 17:20:01 +0300

    fix license

--- src/ruis/widget/group/table_tree_view.hpp
+++ src/ruis/widget/group/table_tree_view.hpp
@@ -1,7 +1,7 @@
 /*
-booh - Double record bookkeeping application
+ruis - GUI framework
 
-Copyright (C) 2025 Ivan Gagis <igagis@gmail.com>
+Copyright (C) 2012-2025  Ivan Gagis <igagis@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -10,11 +10,11 @@ the Free Software Foundation, either version 3 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 /* ================ LICENSE END ================ */
