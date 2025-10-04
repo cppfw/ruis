c7d95dd46fb9a450877263bc7b4e3376e2b2bbc6
Ivan Gagis <igagis@gmail.com>
2025-10-04 23:23:33 +0300

    stuff

--- config/rel.mk
+++ config/rel.mk
@@ -5,10 +5,14 @@ this_cxxflags += -O3
 # TODO: compilation with -O3 currently fails with unjustified "maybe used uninitialized" error
 #       when building with "g++ (Debian 12.2.0-14) 12.2.0",
 #       have't tried it with clang++. Need to remove this warning suppression when GCC is fixed.
-$(info this_cxx = $(this_cxx))
-$(info CXX = $(CXX))
 ifeq ($(this_cxx),g++)
-    this_cxxflags += -Wno-maybe-uninitialized
+    ifeq ($(os),macosx)
+        # On macos, the this_cxx can be set to g++, but it is still clang compiler, becuase
+        # often, on macos the g++ is symlinked to clang++.
+        # Thus, on macos do not suppress the warning, because it is not supported by clang. 
+    else
+        this_cxxflags += -Wno-maybe-uninitialized
+    endif
 endif
 
 # WORKAROUND: on ubuntu jammy dpkg-buildpackage passes -ffat-lto-objects compilation flag
