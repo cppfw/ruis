cdc501b997b273aeed66f058a081cd5a836abb14
Ivan Gagis <igagis@gmail.com>
2026-02-23 14:29:45 +0200

    stuff

--- src/ruis/widget/base/touch/flickable.hpp
+++ src/ruis/widget/base/touch/flickable.hpp
@@ -59,12 +59,7 @@ class flickable :
 	std::deque<touch_move_info> touch_history;
 
 	void push_touch_move_to_history(touch_move_info tm);
-
 	vec2 calculate_touch_velocity_px_per_ms();
-
-	/**
-	 * @return Pixels per ms.
-	 */
 	ruis::vec2 calculate_touch_velocity_for_at_least_3_points_using_ols_method_px_per_ms();
 
 	// inertial scrolling state
