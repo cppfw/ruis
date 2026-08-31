316cec36ea810221d937dd19a66f7cba1da77e78
Ivan Gagis <igagis@gmail.com>
2026-08-31 19:19:24 +0300

    add dialog widget

---
+++ src/ruis/widget/group/touch/dialog.cpp
@@ -0,0 +1,257 @@
+/*
+ruis - GUI framework
+
+Copyright (C) 2012-2026  Ivan Gagis <igagis@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* ================ LICENSE END ================ */
+
+#include "dialog.hpp"
+
+#include "../../../context.hpp"
+#include "../../../layout/abstract_layout.hpp"
+#include "../../label/padding.hpp"
+#include "../../label/rectangle.hpp"
+#include "../../proxy/click_proxy.hpp"
+#include "../../proxy/key_proxy.hpp"
+#include "../../proxy/mouse_proxy.hpp"
+
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+using namespace ruis;
+using namespace ruis::touch;
+
+namespace {
+
+namespace m = ruis::make;
+
+constexpr auto bg_click_proxy_id = "ruis_touch_dialog_bg_click_proxy"sv;
+constexpr auto key_proxy_id = "ruis_touch_dialog_key_proxy"sv;
+
+// Resolve an optional styled value, falling back to the given default when it is undefined.
+template <typename value_type>
+styled<value_type> resolve(
+	const styled<value_type>& v, //
+	const styled<value_type>& def
+)
+{
+	return v.get().is_undefined() ? def : v;
+}
+
+// Build the chrome (dimming background, close-on-click/keypress proxies and the styled panel)
+// surrounding the given dialog content container.
+widget_list make_chrome(
+	utki::shared_ref<ruis::context> c, //
+	utki::shared_ref<ruis::container> content_container, //
+	dialog::parameters params
+)
+{
+	const auto& style = c.get().style();
+
+	auto dim_color = resolve(params.dim_color, style.get_color_dimmed());
+	auto panel_color = resolve(params.panel_color, style.get_color_panel());
+	auto margin = resolve(params.margin, style.get_len_dialog_margin());
+	auto padding = resolve(params.padding, style.get_len_dialog_padding());
+	auto corner_radius = resolve(params.corner_radius, style.get_len_dialog_padding());
+
+	// Dimming background
+	// clang-format off
+	auto dim_bg = m::rectangle(c,
+		{
+			.layout_params{
+				.dims = {ruis::dim::fill, ruis::dim::fill}
+			},
+			.color_params{
+				.color = std::move(dim_color)
+			}
+		}
+	);
+	// clang-format on
+
+	// Background click proxy to close the dialog when clicking outside of the panel
+	// clang-format off
+	auto bg_click_proxy = m::click_proxy(c,
+		{
+			.layout_params{
+				.dims = {ruis::dim::fill, ruis::dim::fill}
+			},
+			.widget_params{
+				.id = std::string(bg_click_proxy_id)
+			}
+		}
+	);
+	// clang-format on
+
+	// Key proxy to close the dialog on Escape key press
+	// clang-format off
+	auto key_proxy = m::key_proxy(c,
+		{
+			.layout_params{
+				.dims = {ruis::dim::fill, ruis::dim::fill}
+			},
+			.widget_params{
+				.id = std::string(key_proxy_id)
+			}
+		},
+		{}
+	);
+	// clang-format on
+
+	// Dialog panel background
+	// clang-format off
+	auto panel_bg = m::rectangle(c,
+		{
+			.layout_params{
+				.dims = {ruis::dim::fill, ruis::dim::fill}
+			},
+			.padding_params{
+				.borders = {std::move(padding)}
+			},
+			.color_params{
+				.color = std::move(panel_color)
+			},
+			.rectangle_params{
+				.corner_radii = {std::move(corner_radius)}
+			}
+		},
+		{
+			std::move(content_container)
+		}
+	);
+	// clang-format on
+
+	// Dialog container with padding providing the margin around the panel
+	// clang-format off
+	auto margin_container = m::padding(c,
+		{
+			.layout_params{
+				.dims = {ruis::dim::fill, ruis::dim::fill}
+			},
+			.padding_params{
+				.borders = {std::move(margin)}
+			}
+		},
+		{
+			// Mouse proxy to consume mouse events inside the dialog,
+			// so that clicks on the panel do not reach the background click proxy.
+			m::mouse_proxy(c,
+				{
+					.layout_params{
+						.dims = {ruis::dim::fill, ruis::dim::fill}
+					},
+					.mouse_proxy_params{
+						.mouse_button_handler = [](auto&, auto&){ return ruis::event_status::consumed; },
+						.mouse_move_handler = [](auto&, auto&){ return ruis::event_status::consumed; }
+					}
+				}
+			),
+			std::move(panel_bg)
+		}
+	);
+	// clang-format on
+
+	return {
+		std::move(dim_bg), //
+		std::move(bg_click_proxy), //
+		std::move(key_proxy), //
+		std::move(margin_container)
+	};
+}
+
+} // namespace
+
+dialog::dialog(
+	utki::shared_ref<ruis::context> context, //
+	all_parameters params,
+	widget_list children
+) :
+	widget( //
+		std::move(context),
+		std::move(params.layout_params),
+		std::move(params.widget_params)
+	),
+	content_wrapping(m::container(
+		this->context,
+		// clang-format off
+		{
+			.layout_params = {
+				.dims = {ruis::dim::fill, ruis::dim::min}
+			},
+			.container_params = [&](){
+				if(!params.container_params.layout){
+					params.container_params.layout = ruis::layout::column;
+				}
+				return std::move(params.container_params);
+			}()
+		},
+		// clang-format on
+		std::move(children)
+	)),
+	// clang-format off
+	container(
+		this->context,
+		{
+			.container_params = {
+				.layout = ruis::layout::pile
+			}
+		},
+		make_chrome(
+			this->context, //
+			this->content_container, //
+			std::move(params.dialog_params)
+		)
+	)
+// clang-format on
+{
+	// Set click handler on the background proxy to close the dialog when clicking outside
+	this->get_widget_as<click_proxy>(bg_click_proxy_id).click_handler = [this](ruis::click_proxy&) {
+		this->close();
+	};
+
+	// Set key handler to close the dialog on Escape key press
+	this->get_widget_as<key_proxy>(key_proxy_id).key_handler =
+		[this](ruis::key_proxy&, const ruis::key_event& e) -> ruis::event_status {
+		if (e.action == ruis::button_action::press && e.combo.key == ruis::key::escape) {
+			this->close();
+			return ruis::event_status::consumed;
+		}
+		return ruis::event_status::propagate;
+	};
+}
+
+void dialog::close()
+{
+	auto self_weak = utki::make_weak_from(*this);
+	this->context.get().post_to_ui_thread([self_weak]() {
+		if (auto self = self_weak.lock()) {
+			self->remove_from_parent();
+		}
+	});
+}
+
+utki::shared_ref<ruis::touch::dialog> ruis::touch::make::dialog(
+	utki::shared_ref<context> context, //
+	dialog::all_parameters params,
+	widget_list children
+)
+{
+	return utki::make_shared<ruis::touch::dialog>(
+		std::move(context), //
+		std::move(params),
+		std::move(children)
+	);
+}
---
+++ src/ruis/widget/group/touch/dialog.hpp
@@ -0,0 +1,127 @@
+/*
+ruis - GUI framework
+
+Copyright (C) 2012-2026  Ivan Gagis <igagis@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* ================ LICENSE END ================ */
+
+#pragma once
+
+#include "../../../util/color.hpp"
+#include "../../../util/content_wrapping.hpp"
+#include "../../../util/length.hpp"
+
+namespace ruis::touch {
+
+/**
+ * @brief Modal dialog widget.
+ * The dialog encapsulates the common (chrome) parts of a modal dialog:
+ * - a dimming background covering the whole area of the dialog,
+ * - a click proxy which closes the dialog when a click outside of the dialog panel occurs,
+ * - a key proxy which closes the dialog when the Escape key is pressed,
+ * - a styled panel background (with margin, padding, color and corner radii) containing the dialog content.
+ * The dialog is meant to be shown on top of the overlay (see ruis::overlay) and by default it fills
+ * the whole area it is placed into.
+ * The dialog content is supplied as a widget_list to the constructor and is laid out according to the
+ * supplied container parameters.
+ */
+// Even though container is inherited privately, the std::enable_shared_from_this base is still accessable via
+// public inheritance of widget. So, the linter complaint is false-positive, suppress it.
+// NOLINTNEXTLINE(bugprone-incorrect-enable-shared-from-this, "false-positive")
+class dialog :
+	public virtual widget, //
+	public content_wrapping,
+	private container
+{
+public:
+	struct parameters {
+		/**
+		 * @brief Color of the dimming background behind the dialog.
+		 * Defaults to the 'color_dimmed' style value if undefined.
+		 */
+		styled<color> dim_color;
+
+		/**
+		 * @brief Color of the dialog panel background.
+		 * Defaults to the 'color_panel' style value if undefined.
+		 */
+		styled<color> panel_color;
+
+		/**
+		 * @brief Margin between the dialog panel and the edge of the dialog area.
+		 * Defaults to the 'len_dialog_margin' style value if undefined.
+		 */
+		styled<length> margin;
+
+		/**
+		 * @brief Padding between the dialog panel background and the dialog content.
+		 * Defaults to the 'len_dialog_padding' style value if undefined.
+		 */
+		styled<length> padding;
+
+		/**
+		 * @brief Corner radii of the dialog panel background.
+		 * Defaults to the 'len_dialog_padding' style value if undefined.
+		 */
+		styled<length> corner_radius;
+	};
+
+	struct all_parameters {
+		layout::parameters layout_params;
+		widget::parameters widget_params;
+		container::parameters container_params;
+		parameters dialog_params;
+	};
+
+	dialog(
+		utki::shared_ref<ruis::context> context, //
+		all_parameters params,
+		widget_list children
+	);
+
+	dialog(const dialog&) = delete;
+	dialog& operator=(const dialog&) = delete;
+
+	dialog(dialog&&) = delete;
+	dialog& operator=(dialog&&) = delete;
+
+	~dialog() override = default;
+
+	/**
+	 * @brief Close the dialog.
+	 * Removes the dialog widget from its parent.
+	 */
+	void close();
+};
+
+namespace make {
+/**
+ * @brief Construct 'dialog' widget.
+ * Default content layout is column.
+ * @param context - ruis context.
+ * @param params - 'dialog' widget parameters.
+ * @param children - contents of the constructed 'dialog' widget.
+ * @return newly constructed 'dialog' widget.
+ */
+utki::shared_ref<ruis::touch::dialog> dialog(
+	utki::shared_ref<context> context, //
+	dialog::all_parameters params,
+	widget_list children
+);
+} // namespace make
+
+} // namespace ruis::touch
