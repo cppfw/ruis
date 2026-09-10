7aa0f72f45878abd80314e56c6dc9e59be4c43f8
Ivan Gagis <igagis@gmail.com>
2026-09-10 14:26:24 +0300

    remove wrapped_widget

--- src/ruis/widget/input/impl/nine_patch_text_field.hpp
+++ src/ruis/widget/input/impl/nine_patch_text_field.hpp
@@ -21,7 +21,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "../../base/wrapped_widget.hpp"
 #include "../../label/nine_patch.hpp"
 #include "../text_field.hpp"
 
